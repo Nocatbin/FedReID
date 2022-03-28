74be0982eb1c6786d726578febb742eaefb512dd
chenyutao <chenyutao@senseauto.com>
2022-03-28 23:22:55 +0800

    pointless commit

--- data_preprocess/prepare_datasets.py
+++ data_preprocess/prepare_datasets.py
@@ -1,7 +1,7 @@
 from glob import glob
-# from scipy.misc import imsave, imread
 from data_preprocess.utils import mkdir
 import os
+import shutil
 
 
 class VIPeR:
@@ -18,20 +18,25 @@ class VIPeR:
         identities = []
         for pid, (cam1, cam2) in enumerate(zip(*cameras)):
             images = []
+            id1 = cam1.split('_')
+            id2 = cam2.split('_')
+            if id1 != id2:
+                print("id not match")
+                continue
             # view-0
-            fname = '{:08d}_{:02d}_{:04d}.jpg'.format(pid, 0, 0)
-            imsave(os.path.join(images_dir, fname), imread(cam1))
-            images.append([fname])
+            # fname = '{:08d}_{:02d}_{:04d}.jpg'.format(pid, 0, 0)
+            # imsave(os.path.join(images_dir, fname), imread(cam1))
+            images.append([cam1])
             # view-1
-            fname = '{:08d}_{:02d}_{:04d}.jpg'.format(pid, 1, 0)
-            imsave(os.path.join(images_dir, fname), imread(cam2))
-            images.append([fname])
+            # fname = '{:08d}_{:02d}_{:04d}.jpg'.format(pid, 1, 0)
+            # imsave(os.path.join(images_dir, fname), imread(cam2))
+            images.append([cam2])
             identities.append(images)
 
         # Save meta information into a json file
         meta = {'name': 'VIPeR', 'shot': 'single', 'num_cameras': 2,
                 'identities': identities}
-        write_json(meta, os.path.join(self.root, 'meta.json'))
+        write_json(meta, os.path.join(self.raw_dir, 'meta.json'))
 
         # Randomly create ten training and test split
         num = len(identities)
