a0c90dc111ea865fe3a41336b378b6ebff61575c
Nocatbin <1500586815@qq.com>
2022-04-08 01:14:58 +0800

    fix data preprocess bug

--- data_preprocess/prepare_datasets.py
+++ data_preprocess/prepare_datasets.py
@@ -25,6 +25,8 @@ class VIPeR:
         assert len(cameras[0]) == len(cameras[1])
         num_test = len(cameras[0]) - (len(cameras[0]) // self.test_ratio)
         identities = []
+        last_id = -999
+        id = 0
         for idx, (cam1, cam2) in enumerate(zip(*cameras)):
             images = []
             # <id>_<view>.bmp
@@ -33,19 +35,21 @@ class VIPeR:
             if id1 != id2:
                 print("id not match")
                 continue
+            if last_id != id1:
+                id += 1
+                last_id = id1
             # 0000_c1.jpg
-            cam1_save_name = '{:04d}_{:03d}_{:03d}.jpg'.format(id1, 1, 0)
-            cam2_save_name = '{:04d}_{:03d}_{:03d}.jpg'.format(id2, 2, 0)
-            # 先存20%的query和gallery作为test，剩下的作为train
+            cam1_save_name = '{:04d}_{:03d}_{:03d}.jpg'.format(id, 1, 0)
+            cam2_save_name = '{:04d}_{:03d}_{:03d}.jpg'.format(id, 2, 0)
             if idx < num_test:
-                train_save_dir = os.path.join(output_train_base, "{:04d}".format(id1))
+                train_save_dir = os.path.join(output_train_base, "{:04d}".format(id))
                 mkdir(train_save_dir)
                 shutil.copyfile(cam1, os.path.join(train_save_dir, cam1_save_name))
                 shutil.copyfile(cam2, os.path.join(train_save_dir, cam2_save_name))
             else:
-                query_save_dir = os.path.join(output_query_base, "{:04d}".format(id1))
+                query_save_dir = os.path.join(output_query_base, "{:04d}".format(id))
                 mkdir(query_save_dir)
-                gallery_save_dir = os.path.join(output_gallery_base, "{:04d}".format(id1))
+                gallery_save_dir = os.path.join(output_gallery_base, "{:04d}".format(id))
                 mkdir(gallery_save_dir)
                 shutil.copyfile(cam1, os.path.join(query_save_dir, cam1_save_name))
                 shutil.copyfile(cam2, os.path.join(gallery_save_dir, cam2_save_name))
@@ -79,15 +83,17 @@ class CUHK01:
         identities = []
         last_id = -999
         idx = 0
+        id = -999
         for image in raw_images:
             images = []
             # <id>_<view>.png
             # <:04d><:03d>.png
-            id = int(os.path.basename(image)[:4])
+            id_tmp = int(os.path.basename(image)[:4])
             view = int(os.path.basename(image)[4:7])
             # 0000_c1.jpg
+            if id_tmp != last_id:
+                id = id_tmp
             save_name = '{:04d}_{:03d}_{:03d}.jpg'.format(id, 1, view)
-            # 先存20%的query和gallery作为test，剩下的作为train
             if idx < num_test:
                 train_save_dir = os.path.join(output_train_base, "{:04d}".format(id))
                 mkdir(train_save_dir)
@@ -103,10 +109,33 @@ class CUHK01:
                     gallery_save_dir = os.path.join(output_gallery_base, "{:04d}".format(id))
                     mkdir(gallery_save_dir)
                     shutil.copyfile(image, os.path.join(gallery_save_dir, save_name))
-                last_id = id
+                last_id = id_tmp
             images.append(image)
             identities.append(images)
             idx += 1
+
+
+def get_id_camera_frame(src_name, dataset):
+    save_name = src_name
+    if dataset == 'MSMT17':
+        # ['0000', 'c14', '0030.jpg']
+        group = src_name.split('_')
+        id = int(group[0])
+        camera = int(group[1][1:])
+        frame = int(group[2][:-4])
+    elif dataset == "Market":
+        # 0001_c1s1_001051_00.jpg
+        group = src_name.split('_')
+        id = int(group[0])
+        camera = int(group[1][1:2])
+        frame = int(group[2])
+    elif dataset == "DukeMTMC-reID":
+        # 0001_c2_f0046182.jpg
+        group = src_name.split('_')
+        id = int(group[0])
+        camera = int(group[1][1:])
+        frame = int(group[2][1:-4])
+    return id, camera, frame
 
 
 def get_save_name(src_name, dataset):
@@ -115,41 +144,29 @@ def get_save_name(src_name, dataset):
         # ['0000', 'c14', '0030.jpg']
         group = src_name.split('_')
         save_name = '{:04d}_{:03d}_{:d}.jpg'.format(int(group[0]), int(group[1][1:]), int(group[2][:-4]))
-        print(save_name)
     elif dataset == "Market":
         # 0001_c1s1_001051_00.jpg
         group = src_name.split('_')
-        if int(group[0]) == -1:
-            print("-1 detected")
         save_name = '{:04d}_{:03d}_{:d}.jpg'.format(int(group[0]), int(group[1][1:2]), int(group[2]))
-        print(save_name)
     elif dataset == "DukeMTMC-reID":
         # 0001_c2_f0046182.jpg
         group = src_name.split('_')
-        save_name = '{:04d}_{:03d}_{:d}.jpg'.format(int(group[0]), int(group[1][1:]), int(group[3][1:-4]))
-        print(save_name)
+        save_name = '{:04d}_{:03d}_{:d}.jpg'.format(int(group[0]), int(group[1][1:]), int(group[2][1:-4]))
     return save_name
 
 
 def prepare_all_big_datasets():
     # datasets=['Market','MSMT17','DukeMTMC-reID','cuhk03-np-detected']
-    datasets = ['Market', 'MSMT17']
+    datasets = ['DukeMTMC-reID']
     for i in datasets:
         data_dir = os.path.abspath('./data/' + i)
-
-        # You only need to change this line to your dataset download path
-        download_path = data_dir
-
-        if not os.path.isdir(download_path):
-            print('please change the download_path')
-
-        save_path = os.path.join(download_path, 'pytorch')
+        save_path = os.path.join(data_dir, 'pytorch')
         if not os.path.isdir(save_path):
             os.mkdir(save_path)
         # -----------------------------------------
         # query
-        query_path = os.path.join(download_path, 'query')
-        query_save_path = os.path.join(download_path, 'pytorch/query')
+        query_path = os.path.join(data_dir, 'query')
+        query_save_path = os.path.join(data_dir, 'pytorch/query')
         if not os.path.isdir(query_save_path):
             os.mkdir(query_save_path)
 
@@ -167,10 +184,10 @@ def prepare_all_big_datasets():
 
         # -----------------------------------------
         # multi-query gt_bbox as query
-        query_path = os.path.join(download_path, 'gt_bbox')
+        query_path = os.path.join(data_dir, 'gt_bbox')
         # for dukemtmc-reid, we do not need multi-query, does not have gt_bbox
         if os.path.isdir(query_path):
-            query_save_path = os.path.join(download_path, 'pytorch/multi-query')
+            query_save_path = os.path.join(data_dir, 'pytorch/multi-query')
             if not os.path.isdir(query_save_path):
                 os.mkdir(query_save_path)
 
@@ -188,8 +205,8 @@ def prepare_all_big_datasets():
 
         # -----------------------------------------
         # gallery / training set
-        gallery_path = os.path.join(download_path, 'bounding_box_test')
-        gallery_save_path = os.path.join(download_path, 'pytorch/gallery')
+        gallery_path = os.path.join(data_dir, 'bounding_box_test')
+        gallery_save_path = os.path.join(data_dir, 'pytorch/gallery')
         if not os.path.isdir(gallery_save_path):
             os.mkdir(gallery_save_path)
 
@@ -207,8 +224,8 @@ def prepare_all_big_datasets():
 
         # ---------------------------------------
         # train_all
-        train_path = download_path + '/bounding_box_train'
-        train_save_path = download_path + '/pytorch/train_all'
+        train_path = data_dir + '/bounding_box_train'
+        train_save_path = data_dir + '/pytorch/train_all'
         if not os.path.isdir(train_save_path):
             os.mkdir(train_save_path)
 
@@ -226,9 +243,11 @@ def prepare_all_big_datasets():
 
         # ---------------------------------------
         # train_val
-        train_path = download_path + '/bounding_box_train'
-        train_save_path = download_path + '/pytorch/train'
-        val_save_path = download_path + '/pytorch/val'
+        last_id = -999
+        id = 0
+        train_path = data_dir + '/bounding_box_train'
+        train_save_path = data_dir + '/pytorch/train'
+        val_save_path = data_dir + '/pytorch/val'
         if not os.path.isdir(train_save_path):
             os.mkdir(train_save_path)
             os.mkdir(val_save_path)
@@ -237,16 +256,21 @@ def prepare_all_big_datasets():
             for name in files:
                 if not name[-3:] == 'jpg' and not name[-3:] == 'png':
                     continue
-                id = name.split('_')
+                id_temp, camera, frame = get_id_camera_frame(name, i)
+                if last_id != id_temp:
+                    id += 1
+                    last_id = id_temp
                 src_path = train_path + '/' + name
-                dst_path = train_save_path + '/' + id[0]
+                dst_path = train_save_path + '/' + '{:04d}'.format(id)
                 if not os.path.isdir(dst_path):
                     os.mkdir(dst_path)
                     # first image of each person(id) is used as val image
-                    dst_path = val_save_path + '/' + id[0]
+                    dst_path = val_save_path + '/' + '{:04d}'.format(id)
                     os.mkdir(dst_path)
-                name = get_save_name(name, i)
-                shutil.copyfile(src_path, dst_path + '/' + name)
+                save_name = '{:04d}_{:03d}_{:d}.jpg'.format(id, camera, frame)
+                if os.path.isfile(dst_path + '/' + save_name):
+                    save_name = '{:04d}_{:03d}_{:d}.jpg'.format(id, camera, frame + 1)
+                shutil.copyfile(src_path, dst_path + '/' + save_name)
 
 
 if __name__ == '__main__':
@@ -264,9 +288,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
     print(args.dataset_dir)
 
-    prepare_all_big_datasets()
+    # prepare_all_big_datasets()
     # '3dpes', 'cuhk01', 'cuhk02', 'ilids', 'prid', 'viper'
-    for dataset in ['viper', 'cuhk01']:
+    for dataset in ['cuhk01']:
         if dataset == 'viper' or dataset == 'VIPeR':
             data = VIPeR("./data")
             data.prepare()
